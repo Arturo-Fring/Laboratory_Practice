4a86a9bdb52d1c99a18da1afca63bb48ba99999e
Arthur_Tsopin <arturiovv33@gmail.com>
2025-11-06 16:26:15 +0700

    Не для проверки. Сохранение версии 06/11

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,7 +1,47 @@
+#include <stdint.h>
+#include "init.h"
+#include "Interrupt.h"
+
+volatile uint8_t btnCount = 0;
+volatile uint8_t flag = 0;
+
+// Привет всем из первой лабораторной работы!
 int main(void)
 {
+    RCC_INIT();
+    ITR_Init();
+
+    /*Настройка тактирования, светодиода */
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN | RCC_AHB1ENR_GPIOAEN);
+
+    SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOBEN);
+    SET_BIT(GPIOB->MODER, GPIO_MODER_MODE7_0); /* 01 */
+
+    /*-----------MCO2------------------------*/
+    // Необхоидимо настроить пины на выход
+    SET_BIT(GPIOC->MODER, GPIO_MODER_MODER9_1); // Alt. func
+
+    SET_BIT(GPIOC->OSPEEDR, GPIO_OSPEEDR_OSPEED9_Msk); // Нужна высокая ск., 11.
+    // CLEAR_BIT(GPIOC->AFR[1], GPIO_AFRH_AFSEL9);
+    MODIFY_REG(GPIOC->AFR[1], GPIO_AFRH_AFSEL9_Msk, 0x0); // выход MCO2.
+
+    /*-------------MCO1--------------------*/
+    // PA8, AF0 (0000)
+
+    SET_BIT(GPIOA->MODER, GPIO_MODER_MODE8_1);
+    SET_BIT(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED8);
+    CLEAR_BIT(GPIOA->AFR[1], GPIO_AFRH_AFSEL8);
 
     while (1)
     {
+        if (flag)
+        {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS7);
+        }
+
+        else
+        {
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR7);
+        }
     }
 }
--- build_F429ZI/Interrupt.d
+++ build_F429ZI/Interrupt.d
@@ -1,4 +1,5 @@
-build_F429ZI/main.o: Core/Src/main.c Core/Inc/init.h \
+build_F429ZI/Interrupt.o: Core/Src/Interrupt.c Core/Inc/Interrupt.h \
+ Core/Inc/init.h \
  Core/Inc/../../CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h \
  CMSIS/Include/../include/core_cm4.h \
  CMSIS/Include/../include/cmsis_version.h \
@@ -7,8 +8,8 @@ build_F429ZI/main.o: Core/Src/main.c Core/Inc/init.h \
  CMSIS/Include/../include/mpu_armv7.h \
  CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h \
  CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h \
- CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h \
- Core/Inc/Interrupt.h Core/Inc/init.h
+ CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h
+Core/Inc/Interrupt.h:
 Core/Inc/init.h:
 Core/Inc/../../CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h:
 CMSIS/Include/../include/core_cm4.h:
@@ -19,5 +20,3 @@ CMSIS/Include/../include/mpu_armv7.h:
 CMSIS/Devices/STM32F4xx/Inc/system_stm32f4xx.h:
 CMSIS/Devices/STM32F4xx/Inc/stm32f4xx.h:
 CMSIS/Devices/STM32F4xx/Inc/STM32F429ZI/stm32f429xx.h:
-Core/Inc/Interrupt.h:
-Core/Inc/init.h:
